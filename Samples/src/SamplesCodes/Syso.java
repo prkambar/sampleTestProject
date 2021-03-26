9cf5fb4c9f0a32ccbc174b6ccf3d81b16fe2cb8b
Prasad Kambar <81404721+prkambar@users.noreply.github.com>
2021-03-26 11:42:14 +0530

    Update Syso.java

--- Samples/src/SamplesCodes/Syso.java
+++ Samples/src/SamplesCodes/Syso.java
@@ -5,7 +5,7 @@ public class Syso {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	 System.out.println("3rd class to github");
-	 
+	 System.out.println("3rd class to edited on github");
 	}
 
 }
