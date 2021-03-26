75bdea1aaadc40481164bf811106516ed6e3a4f7
prkambar <prkambar@Tejasit-PC8.TISPL.COM>
2021-03-26 11:40:47 +0530

    3rd class to github

---
+++ Samples/src/SamplesCodes/Syso.java
@@ -0,0 +1,11 @@
+package SamplesCodes;
+
+public class Syso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 System.out.println("3rd class to github");
+	 
+	}
+
+}
